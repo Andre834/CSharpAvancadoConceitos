4e3e5020cd01c9c1128d3597600b9ab7d3b7d009
Andre Santos <63032209+Andre834@users.noreply.github.com>
2023-03-15 02:23:20 -0300

    Adicionar arquivos de projeto.

---
+++ Tuplas/MetodosComTuplas/MetodoComRetornoTipoTupla.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuplas.MetodosComTuplas
+{
+    public static class MetodoComRetornoTipoTupla
+    {
+        public static (string, int) GetUsuario()
+        {
+            return("Paulo Santos", 6);
+        }
+    }
+}
---
+++ Tuplas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tuplas.MetodosComTuplas;
+
+namespace Tuplas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aprendo conceitos avançado de c#: TUPLAS!");
+            Console.WriteLine("Chamando uma função que tem como retorno uma tupla: " + MetodoComRetornoTipoTupla.GetUsuario());
+
+            
+        }
+    }
+}

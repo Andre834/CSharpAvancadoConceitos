829f996290c8b894b5f9addf9a3203417fba4a57
Andre Santos <63032209+Andre834@users.noreply.github.com>
2023-03-18 23:18:00 -0300

    correspondencia de padroes

--- CorrespondenciaDePadroes/PatternMatchingExample.cs
+++ CorrespondenciaDePadroes/PatternMatchingExample.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace CorrespondenciaDePadroes
 {
-    internal class PatternMatchingExample
+    /*
+     No C# 7, agora temos a capacidade de usar a correspondência de padrões. Usando padrões, podemos testar se um valor tem uma determinada forma e, em caso afirmativo, trabalhar com as informações dessa forma correspondente.
+
+     verdade, você já está usando algoritmos de correspondência de padrões quando usa as instruções if e switch para testar valores. Se as instruções corresponderem, você pega o valor correspondente e extrai suas informações.
+
+    No C# 7, você pode usar novos elementos de sintaxe que estendem as instruções is e switch com as quais você já está familiarizado. Vamos começar criando uma nova classe chamada PatternMatchingExample e adicionando nosso código a essa classe.
+     
+     */
+    public class PatternMatchingExample
     {
+        public enum UniversityCourses { Matemática, Química, Anatomia, LifeSkills }
+        public enum UniversityDegree { BA, BSc }
     }
 }
